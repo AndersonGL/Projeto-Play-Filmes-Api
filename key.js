e4afb8d9d22038cad8ef525e29ff050b9fedfaa3
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-06-26 23:25:43 -0300

    update

--- key.js
+++ key.js
@@ -1,1 +1,2 @@
-const key = "d6d5845c"; //Aqui irá a sua chave de API entre aspas "".
+const key = "d6d5845c"; //Aqui irá a sua chave de API entre aspas, colocando a mesma no arquivo script.js "".
+
